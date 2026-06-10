567c6ff708c13ed84229580f3247327c816020eb
MicheleScattola <michi.scattola@gmail.com>
2026-06-10 16:30:46 +0200

    added torch with cuda12.8 compatibility for GTX 1080ti

--- plot_data.py
+++ plot_data.py
@@ -16,13 +16,13 @@ def plot(PROC,OUTDIR,title):
 
   fig, ax = plt.subplots(2, 2, figsize=(8,8))
   fig.suptitle(title,fontweight='bold')
-  ax[0,0].hist(data[:,0],color=colors[0],label=labels[0])
+  ax[0,0].hist(data[:,0],color=colors[0],label=labels[0],bins=20)
   ax[0,0].set(title=r'$x_{vis} = E_{track} / E_\tau$',xlabel='x',ylabel='Events')
-  ax[0,1].hist(data[:,1],color=colors[1],label=labels[1])
+  ax[0,1].hist(data[:,1],color=colors[1],label=labels[1],bins=20)
   ax[0,1].set(title=r'$E_{cone}$',xlabel=r'$E_{cone}$ [GeV]',ylabel='Events')
-  ax[1,0].hist(data[:,2],color=colors[2],label=labels[2])
+  ax[1,0].hist(data[:,2],color=colors[2],label=labels[2],bins=20)
   ax[1,0].set(title=r'$f_{had}$',xlabel=r'$f_{had} = E_{HCAL} / E_{TOT}$',ylabel='Events')
-  ax[1,1].hist(data[:,3],color=colors[3],label=labels[3])
+  ax[1,1].hist(data[:,3],color=colors[3],label=labels[3],bins=20)
   ax[1,1].set(title=r'$\eta$',xlabel=r'$\eta_{track}$',ylabel='Events')
 
   fig.tight_layout()
@@ -34,7 +34,7 @@ def plot(PROC,OUTDIR,title):
 
 # MAIN
 if __name__ == "__main__":
-  plot('pi_LH',OUTDIR,r'$\pi$ LH decays')
-  plot('pi_RH',OUTDIR,r'$\pi$ RH decays')
-  plot('rho_LH',OUTDIR,r'$\rho$ LH decays')
-  plot('rho_RH',OUTDIR,r'$\rho$ RH decays')
+  plot('pi_LH',OUTDIR,r'$\tau \to \pi\nu$ LH decays')
+  plot('pi_RH',OUTDIR,r'$\tau \to \pi\nu$ RH decays')
+  plot('rho_LH',OUTDIR,r'$\tau \to \rho\nu \to \pi \pi^0 \nu$ LH decays')
+  plot('rho_RH',OUTDIR,r'$\tau \to \rho\nu \to \pi \pi^0 \nu$ RH decays')
